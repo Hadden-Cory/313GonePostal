8c7dff315bd0d2fa74eb4b10b0037d277e2a6deb
Mona Lisa <had17007@byui.edu>
2019-03-09 17:09:24 -0700

    Adding Results page

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 8000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+// express()
+//   //.use(express.static(path.join(__dirname, 'public')))
+//   .use(express.static('public'))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 
   //TODO: Fill this out!
   function calculateRate(){
