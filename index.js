d204bf5cc3797ff82d2ad5fd68bc5aa7fad4b9d1
Mona Lisa <had17007@byui.edu>
2019-03-10 00:55:43 -0700

    Finished

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/313GonePostal.html')
 })
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/form", function(request, response) {
-  console.log('Data passed via get');
-});
+app.get("/form", getRate);
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
@@ -25,78 +26,80 @@ function getRate(request, response) {
 function calculateRate(response, mailMethod, weight) {
   let result = 0;
   console.log("rate called");
-  // weight = weight*16;
+  
+  let lbWeight = weight/16;
 
   if (mailMethod == "stamped") {
-    if (weight < 1) {
-      result = 0.55;
+    if (weight < 1 && weight > 0) {
+      result = "0.55";
     } else if (weight < 2) {
-      result = 0.70;
+      result = "0.70";
     } else if (weight < 3) {
-      result = 0.85;
+      result = "0.85";
     } else if (weight < 3.5) {
-      result = 1;
+      result = "1.00";
     } else {
-      alert("Weight is outside the approved range for stamped letters");
+      result="was not calculated was not becasue the weight enter is outside the approved range for stamped letters";
     }
   } else if (mailMethod == "metered") {
-    if (weight < 1) {
-      result = 0.5;
+    if (weight < 1 && weight > 0) {
+      result = "0.50";
     } else if (weight < 2) {
-      result = 0.65;
+      result = "0.65";
     } else if (weight < 3) {
-      result = 0.8;
+      result = "0.80";
     } else if (weight < 3.5) {
-      result = 0.95;
+      result = "0.95";
     } else {
-      alert("Weight is outside the approved range for metered letters");
+      result="was not calculated was not becasue the weight enter is outside the approved range for metered letters";
     }
   } else if (mailMethod == "large") {
-    if (weight < 1) {
-      result = 1;
+    if (weight < 1 && weight > 0) {
+      result = "1.00";
     } else if (weight < 2) {
-      result = 1.15;
+      result = "1.15";
     } else if (weight < 3) {
-      result = 1.3;
+      result = "1.30";
     } else if (weight < 4) {
-      result = 1.45;
+      result = "1.45";
     } else if (weight < 5) {
-      result = 1.6;
+      result = "1.60";
     } else if (weight < 6) {
-      result = 1.75;
+      result = "1.75";
     } else if (weight < 7) {
-      result = 1.9;
+      result = "1.90";
     } else if (weight < 8) {
-      result = 2.05;
+      result = "2.05";
     } else if (weight < 9) {
-      result = 2.2;
+      result = "2.20";
     } else if (weight < 10) {
-      result = 2.35;
+      result = "2.35";
     } else if (weight < 11) {
-      result = 2.5;
+      result = "2.50";
     } else if (weight < 12) {
-      result = 2.65;
+      result = "2.65";
     } else if (weight < 13) {
-      result = 2.8;
+      result = "2.80";
     } else {
-      alert("Weight is outside the approved range for large envolopes");
+      result="was not calculated was not becasue the weight enter is outside the approved range for large envolopes";
     }
   } else if (mailMethod == "firstClass") {
-    if (weight < 4) {
-      result = 3.66;
+    if (weight < 4 && weight > 0) {
+      result = "3.66";
     } else if (weight < 8) {
-      result = 4.39;
+      result = "4.39";
     } else if (weight < 12) {
-      result = 5.19;
+      result = "5.19";
     } else if (weight < 13) {
-      result = 5.71;
+      result = "5.71";
     } else {
-      alert("Weight is outside the approved range for large envolopes");
+      result="was not calculated was not becasue the weight enter is outside the approved range for large envolopes";
     }
   }
 
   const params = {
     weight: weight,
+    lbWeight: lbWeight,
     rate: result,
     mailMethod: mailMethod
   };
